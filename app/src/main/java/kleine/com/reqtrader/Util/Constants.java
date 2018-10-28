8a1c987ef1bb8eb2c66fbb663275ad49dc7edec0
Jochem <=>
2018-10-28 11:16:50 +0100

    recommit

--- app/src/main/java/kleine/com/reqtrader/Util/Constants.java
+++ app/src/main/java/kleine/com/reqtrader/Util/Constants.java
@@ -4,9 +4,10 @@ public class Constants {
 
     public static final String SORT_MARKET_CAP = "market_cap";
     public static final String CMC_BASE_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/";
-    public static final String CMC_HEADER = "X-CMC_PRO_API_KEY";
     public static final String API_KEY = "e8ac0f1b-85ac-4774-85bd-b07b2573cad2";
 
 
     public static final String UNIT_USD = "USD";
+    
+    public String temp;
 }
